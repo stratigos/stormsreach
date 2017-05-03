b3bdd78a83ffccf992b9db0a7239c66f1453e263
stratigos <stratigos@users.noreply.github.com>
2017-05-03 15:40:32 -0400

    Initialize React/Redux application backbone.

---
+++ app/app.js
@@ -0,0 +1,24 @@
+const React    = 'react'
+const ReactDOM = 'react-dom'
+require('./index.css')
+
+/**
+ * Define component.
+ */
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        Welcome to stormsreach.com! The online community for the greater Bay of Storms and Elysium isles area.
+      </div>
+    )
+  }
+}
+
+/**
+ * Render component to the DOM.
+ */
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
