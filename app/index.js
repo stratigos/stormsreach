685ab9fa59940627c57ff861cbfa7776ec92e237
stratigos <stratigos@users.noreply.github.com>
2017-05-06 09:41:53 -0400

    Fix require statement for loading index.css.

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
-require('./index.css');
+require('./assets/stylesheets/index.css');
 
 /**
  * Render component to the DOM.
