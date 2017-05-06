6f37ee99570d40ed677db03aa5c4179522d30b6c
stratigos <stratigos@users.noreply.github.com>
2017-05-06 12:53:36 -0400

    Prepare application for deploy to Heroku.
    * Unignore the dist dir and build application there.
    * Add express JS and create express server.
    * Add npm command for running production build in localdev.
    * Create Heroku Procfile.

---
+++ server.js
@@ -0,0 +1,10 @@
+/**
+ * Production application server.
+ */
+var express = require('express');
+var app     = express();
+
+app.use(express.static(__dirname + '/dist/'));
+
+app.listen(process.env.PORT || 8080);
+
