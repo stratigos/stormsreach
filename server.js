5f1e6ddd9b29baa3b681cc365a68d4a836cacbce
stratigos <stratigos@users.noreply.github.com>
2017-05-07 01:43:54 -0400

    Create production build (Crafting page).
    * Update express script to forward all requests to entry script.
    * Create demo data of list of crafters.

--- server.js
+++ server.js
@@ -6,5 +6,11 @@ var app     = express();
 
 app.use(express.static(__dirname + '/dist/'));
 
-app.listen(process.env.PORT || 8080);
+// Forward all URL paths to the main index. Otherwise, routes can not be
+//  reached directly, and hard refreshes to deep links will not load the
+//  application.
+app.get('*', function(req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+});
 
+app.listen(process.env.PORT || 8080);
