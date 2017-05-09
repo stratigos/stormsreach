79bba0f65b3c6b3c4ecba41ad5a2f1a2f43a01c0
stratigos <stratigos@users.noreply.github.com>
2017-05-09 11:45:38 -0400

    Implement initial tests with Jest.
     * Add enzyme library for rendering React components.
     * Add react-test-renderer library to assist enzyme.
     * Update About component to use container div like rest of the app.
     * Create initial tests for About component.

--- app/components/About.js
+++ app/components/About.js
@@ -5,7 +5,7 @@ import React from 'react';
  */
 const About = () => {
   return(
-    <p>About this site...</p>
+    <div className='about-container'>About this site...</div>
   );
 }
 
---
+++ test/components/About.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import About from '../../app/components/About';
+
+describe('<About /> Component', () => {
+
+  it('should be a stateless functional component', () => {
+    expect(typeof About).toEqual('function');
+  });
+
+  it('renders a containing div', () => {
+    expect(shallow(<About />).is('.about-container')).toBe(true);
+  });
+
+  it('displays text about the site', () => {
+    expect(shallow(<About />).text()).toMatch(/About this site/);
+  });
+
+});
