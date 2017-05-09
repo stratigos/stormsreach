3f7e9294531a8e28f677c3806f8d91677908a8bd
stratigos <stratigos@users.noreply.github.com>
2017-05-09 12:55:25 -0400

    Create tests for NavProfile Component.

---
+++ test/components/NavProfile.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Link } from 'react-router-dom';
+
+import NavProfile from '../../app/components/NavProfile';
+
+describe('<Nav /> Component', () => {
+
+  describe('as a stateless functional component', () => {
+
+    it('should be a function', () => {
+      expect( typeof NavProfile ).toEqual('function');
+    });
+
+  });
+
+  it('should not have propTypes defined', () => {
+    expect( shallow(<NavProfile />).propTypes ).toBeUndefined();
+  });
+
+  it('should not have defaultProps', () => {
+    expect( shallow(<NavProfile />).defaultProps ).toBeUndefined();
+  });
+
+  it('should render a Link element', () => {
+    const testNavProfile = shallow(<NavProfile />);
+
+    expect( testNavProfile.find('Link').length ).toEqual(1);
+  });
+
+});
