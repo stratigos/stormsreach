c5e2c55ffc821df9bb2cdfdb5014ee1f0451cbbe
stratigos <stratigos@users.noreply.github.com>
2017-05-09 12:51:39 -0400

    Add clarity to About Component tests.

--- test/components/About.test.js
+++ test/components/About.test.js
@@ -5,16 +5,28 @@ import About from '../../app/components/About';
 
 describe('<About /> Component', () => {
 
-  it('should be a stateless functional component', () => {
-    expect(typeof About).toEqual('function');
+  describe('as a stateless functional component', () => {
+
+    it('should be a function', () => {
+      expect( typeof About ).toEqual('function');
+    });
+
+  });
+
+  it('should not have propTypes defined', () => {
+    expect( shallow(<About />).propTypes ).toBeUndefined();
+  });
+
+  it('should not have defaultProps', () => {
+    expect( shallow(<About />).defaultProps ).toBeUndefined();
   });
 
   it('renders a containing div', () => {
-    expect(shallow(<About />).is('.about-container')).toBe(true);
+    expect( shallow(<About />).is('.about-container') ).toBe(true);
   });
 
   it('displays text about the site', () => {
-    expect(shallow(<About />).text()).toMatch(/About this site/);
+    expect( shallow(<About />).text() ).toMatch(/About this site/);
   });
 
 });
