62568aaa5c691d8a69d45c21fa9ecd5ea62dd769
stratigos <stratigos@users.noreply.github.com>
2017-05-09 21:33:48 -0400

    Add tests for HomepageLink Component.

---
+++ test/components/HomepageLink.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+// import { Link } from 'react-router-dom'; // TODO NEED THIS?
+
+import HomepageLink from '../../app/components/HomepageLink';
+
+describe('<HomepageLink /> Component', () => {
+
+  describe('as a stateless functional component', () => {
+
+    it('should be a function', () => {
+      expect( typeof HomepageLink ).toEqual('function');
+    });
+
+  });
+
+  it('should not have propTypes defined', () => {
+    expect( shallow(<HomepageLink />).propTypes ).toBeUndefined();
+  });
+
+  it('should not have defaultProps', () => {
+    expect( shallow(<HomepageLink />).defaultProps ).toBeUndefined();
+  });
+
+  it('should render its containing div', () => {
+    expect( shallow(<HomepageLink />).is('div.homepage-link-container') ).toBe(true);
+  });
+
+  it('should render a Link element for the home page', () => {
+    const testHomepageLink = shallow(<HomepageLink />);
+    const childLink        = testHomepageLink.find('Link');
+
+
+
+    expect( childLink.length ).toEqual(1);
+    expect( childLink.props().to ).toEqual('/');
+  });
+
+});
