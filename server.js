f00a9883f06482aded6129e4ddead35024759602
stratigos <stratigos@users.noreply.github.com>
2017-05-13 18:07:47 -0400

    Fix production API request by providing webserver access to data directory.

--- server.js
+++ server.js
@@ -4,7 +4,10 @@
 var express = require('express');
 var app     = express();
 
+// Set base path for application.
 app.use(express.static(__dirname + '/dist/'));
+// Set base path to `/data` for fetching application JSON responses.
+app.use('/data', express.static(__dirname + '/app/data/'));
 
 // Forward all URL paths to the main index. Otherwise, routes can not be
 //  reached directly, and hard refreshes to deep links will not load the
