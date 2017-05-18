a0629c5e7e7d09352f5a2da397e6f94460af2c71
stratigos <stratigos@users.noreply.github.com>
2017-05-18 17:58:13 -0400

    Send 'Transfer Encoding' HTTP Header in production
     * This assists with TCP Slow Start issues on mobile or down in the subway :).

--- server.js
+++ server.js
@@ -4,6 +4,19 @@
 var express = require('express');
 var app     = express();
 
+/**
+ * Server middleware.
+ */
+
+// Set HTTP Headers.
+app.use(function(req, res, next) {
+  // The `Transfer-Encoding: chunked` HTTP header streams the application
+  //  content, to avoid potential ~200ms lag from TCP Slow Start.
+  res.setHeader('Transfer-Encoding', 'chunked');
+
+  next();
+});
+
 // Set base path for application.
 app.use(express.static(__dirname + '/dist/'));
 // Set base path to `/data` for fetching application JSON responses.
@@ -12,8 +25,13 @@ app.use('/data', express.static(__dirname + '/app/data/'));
 // Forward all URL paths to the main index. Otherwise, routes can not be
 //  reached directly, and hard refreshes to deep links will not load the
 //  application.
+// Using the wildcard essentially pushes responsibility of routing onto
+//  React Router.
 app.get('*', function(req, res) {
   res.sendFile(__dirname + '/dist/index.html');
 });
 
+/**
+ * Listen for requests.
+ */
 app.listen(process.env.PORT || 8080);
