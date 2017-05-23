e3b464d37d8df8b55264a13cb8a127444bf68e52
stratigos <stratigos@users.noreply.github.com>
2017-05-23 11:00:07 -0400

    Fix typo in SET_RENTALS Action Creator.

--- app/actions/set_rentals.js
+++ app/actions/set_rentals.js
@@ -1,12 +1,11 @@
 import { SET_RENTALS } from '../constants/action_types';
 
-
 const setRentals = (rentals_json) => (
 
   let rentals = rentals_json.rentals;
 
   return {
-    type: SET_AVATARS,
+    type: SET_RENTALS,
     payload: { rentals }
   }
 );
