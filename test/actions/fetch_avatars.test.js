d5254f70093c67f015af279a7f150e0b81a95dc2
Todd Morningstar <techarchist@gmail.com>
2018-01-14 15:13:10 -0500

    Add test for Fetch Avatars ActionCreator

---
+++ test/actions/fetch_avatars.test.js
@@ -0,0 +1,20 @@
+import fetchAvatarsAction from '../../app/actions/fetch_avatars'
+import { API_ENDPOINT_AVATARS } from '../../app/constants/api_endpoints';
+import * as actionTypes from '../../app/constants/action_types'
+
+describe('Fetch Avatars ActionCreator', () => {
+
+  it('creates an action to request fetching of Avatars with action to perform on success', () => {
+
+    const expectedAction = {
+      type: actionTypes.FETCH_AVATARS,
+      payload: {
+        url: API_ENDPOINT_AVATARS,
+        success: actionTypes.SET_AVATARS
+      }
+    };
+
+    expect(fetchAvatarsAction()).toEqual(expectedAction);
+  });
+
+});
