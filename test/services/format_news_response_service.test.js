5920fef3b23efde04a69275d5217fdef56c35f5a
Todd Morningstar <techarchist@gmail.com>
2018-03-08 17:05:39 -0500

    Refactor formatNewsResponseService test
     * declare expected values just once

--- test/services/format_news_response_service.test.js
+++ test/services/format_news_response_service.test.js
@@ -5,32 +5,29 @@ import {
   defaultEmptyNews
 } from '../../app/lib/services/format_news_response_service';
 
-describe('function defaultEmptyNews', () => {
+describe('function defaultEmptyNews()', () => {
+
+  const assertedDefaultEmptyNewsItems = defaultEmptyNews();
 
   it('returns an array', () => {
-    const assertedDefaultEmptyNewsItems = defaultEmptyNews();
-
     expect(assertedDefaultEmptyNewsItems.constructor).toBe(Array);
   });
 
   describe('array contents', () => {
 
-    it('contains just one object', () => {
-      const assertedDefaultEmptyNewsItems = defaultEmptyNews();
+    const expectedDefaulEmptyNewsItem = {
+      newsItemId: '0',
+      newsItem: {
+        id: '0',
+        content: 'No news returned - try again later.'
+      }
+    };
 
+    it('contains just one object', () => {
       expect(assertedDefaultEmptyNewsItems.length).toBe(1);
     });
 
     it('has an object that conforms to NewsItem props', () => {
-      const assertedDefaultEmptyNewsItems = defaultEmptyNews();
-      const expectedDefaulEmptyNewsItem = {
-        newsItemId: '0',
-        newsItem: {
-          id: '0',
-          content: 'No news returned - try again later.'
-        }
-      };
-
       expect(assertedDefaultEmptyNewsItems[0]).toEqual(expectedDefaulEmptyNewsItem);
     });
 
