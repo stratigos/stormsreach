acffaf20d2f3c55b2f3c06c1399696352c47135d
Todd Morningstar <techarchist@gmail.com>
2018-03-08 22:48:09 -0500

    Add tests for formatNewsResponseService.formatNewsItemContent

--- test/services/format_news_response_service.test.js
+++ test/services/format_news_response_service.test.js
@@ -34,3 +34,32 @@ describe('function defaultEmptyNews()', () => {
   });
 
 });
+
+describe('function formatNewsItemContent()', () => {
+
+  /**
+   * "Source" is a nomenclature of the response body from the ElasticSearch
+   *  API response, and represents a "hit" of a search result set.
+   */
+  const testSource = {
+    '@timestamp': '2018-03-06T05:03:05.000Z',
+    Killer: 'Lord British',
+    LocationEvent: 'MonsterKilledByPlayer',
+    PlayerName: 'Anonymous',
+    SceneName: 'Novia_R6_City_StormsReach',
+    Victim: 'Sheep',
+    timestamp: '%{month} %{day} %{hour}:00:00',
+    xpos: '119.86',
+    ypos: '38.00',
+    zpos: '85.65'
+  };
+
+  const expectedNewsItemContent = `${testSource.Victim} killed by ${testSource.Killer} on ${testSource['@timestamp']}`;
+
+  const assertededNewsItemContent = formatNewsItemContent(testSource);
+
+  it('creates a String with player name, target, and timestamp', () => {
+    expect(assertededNewsItemContent).toEqual(expectedNewsItemContent);
+  });
+
+});
